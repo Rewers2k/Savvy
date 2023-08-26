7d26384302e9b27edd2450e1f945d314f247507f
Rewers2k <rewers1200@gmail.com>
2023-08-26 06:03:39 +0200

    Initialisation of the backend project and connection to the database

---
+++ backend/app/config.py
@@ -0,0 +1,44 @@
+from typing import Any, List, Dict, Union, Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import AnyHttpUrl, PostgresDsn
+from pydantic.functional_validators import field_validator
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Savvy"
+    API_V1_STR: str = "/api/v1"
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+    @classmethod
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
+    @classmethod
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg2",
+            username=values.data.get("POSTGRES_USER"),
+            password=values.data.get("POSTGRES_PASSWORD"),
+            host=values.data.get("POSTGRES_SERVER"),
+            path=values.data.get('POSTGRES_DB'),
+        )
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
